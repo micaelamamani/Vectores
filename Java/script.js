2daf7a15b7bcad681e162bc82a56fc941208140d
micaelamamani <micamamaniquispe@gmail.com>
2026-04-14 21:20:25 -0300

    funcion mostrar

--- Java/script.js
+++ Java/script.js
@@ -1,5 +1,27 @@
 let personas= [
     {
         nombre: "Juan Carlos", edad: 48, altura: 1.85 
-    }
-]
+    },
+];
+
+document.getElementById('btnIngresar').addEventListener('click', nuevaPersona);
+
+function nuevaPersona(){
+    let anios, alt, nom;
+    anios = parseInt(document.getElementById('IDEdad').value);
+    alt = parseFloat(document.getElementById('IDAltura').value);
+    nom = String(document.getElementById('IDNom').value).toUpperCase();
+ 
+ let objPersona = {
+    nombre: nom,
+    edad: anios,
+    altura: alt
+}
+
+ personas.push(objPersona);
+ console.log(personas);
+ document.getElementById('IDNom').Value = "";
+ document.getElementById('IDEdad').Value = "";
+ document.getElementById('IDAltura').Value = "";
+};
+
